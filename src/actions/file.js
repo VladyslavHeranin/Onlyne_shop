67b751c9431ae888d5db550c245b7ab2cdeda651
VladyslavHeranin <geraninvladik4@gmail.com>
2021-06-25 14:23:50 +0300

    update

--- src/actions/file.js
+++ src/actions/file.js
@@ -6,7 +6,7 @@ import { setList } from "../reducers/userRed"
 export function createDir(id, name, imgURL, size, weidht, count, discription) {
     return async dispatch => {
         try {
-            const response = await axios.post(`https://app-23456789.herokuapp.com//api/group/groups`, {
+            const response = await axios.post(`https://app-23456789.herokuapp.com/api/group/groups`, {
                 id, name, imgURL, size, weidht, count, discription
             })
 
